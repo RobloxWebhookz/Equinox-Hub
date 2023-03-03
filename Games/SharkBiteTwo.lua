a1418478edaa67ef045858c63391b41cc6711b0b
RobloxWebhookz <121607654+RobloxWebhookz@users.noreply.github.com>
2023-03-03 16:07:08 -0600

    Update SharkBiteTwo.lua

--- Games/SharkBiteTwo.lua
+++ Games/SharkBiteTwo.lua
@@ -1,8 +1,19 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
 
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local GUI = Mercury:Create{
+    Name = "Mercury",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+}
 
-local Tab = GUI:Tab{
+
+local Boats = GUI:Tab{
+	Name = "New Tab",
+	Icon = "rbxassetid://8569322835"
+}
+
+local Render = GUI:Tab{
 	Name = "New Tab",
 	Icon = "rbxassetid://8569322835"
 }
